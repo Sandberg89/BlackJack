82af6ac997b111d6a5ca757271804bb1acac75df
Linus Sandberg <Linus.Sandberg@visma.com>
2020-09-10 16:58:57 +0200

    added class

---
+++ BlackJackGame/LogicLayer/Entities/Hand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicLayer.Entities
+{
+    public class Hand
+    {
+    }
+}
---
+++ BlackJackGame/LogicLayer/Entities/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicLayer.Entities
+{
+    public class Player
+    {
+        private bool isFinished = false;
+
+        public bool IsFinished { get; set; }
+
+        public List<Card> playerCards { get; set; }
+
+        public bool IsThick 
+        { 
+            get; 
+            set; 
+        }
+
+    }
+}
